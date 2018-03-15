9f19463f257ce4e5a368319aa21d44912c107c4c
Aric Alves <aric.alves@yahoo.com>
2018-03-15 13:31:25 -0700

    (feat) Add body-parser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import { json as parseJSON } from 'body-parser';
 
 const app = express();
+
+app.use(parseJSON());
 
 app.all('*', (req, res) => res.sendStatus(200));
 
