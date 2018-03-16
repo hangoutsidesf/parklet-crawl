baa37b562645d299522e27f387ef60cf642986a9
Aric Alves <aric.alves@yahoo.com>
2018-03-16 16:25:15 -0700

    (test) Add random index to test any parklet obj

--- spec/lib/data-sf/index.spec.js
+++ spec/lib/data-sf/index.spec.js
@@ -4,10 +4,11 @@ describe('Data SF', () => {
   test('Should fetch blob of parklet data', () => (
     getParklets().then(({ data }) => expect(data.length).toBeGreaterThan(0))
   ));
-  test('Should prune unecessary keys from parklet obj', () => (
-    getParklets()
-      .then(({ data }) => data[0])
+  test('Should prune unecessary keys from parklet obj', () => {
+    const index = Math.floor(Math.random() * 31); // Pick a random parklet from the list.
+    return getParklets()
+      .then(({ data }) => data[index])
       .then(parklet => parseParklet(parklet))
-      .then(prunedParklet => expect(Object.keys(prunedParklet).length).toBe(7))
-  ));
+      .then(prunedParklet => expect(Object.keys(prunedParklet).length).toBe(7));
+  });
 });
