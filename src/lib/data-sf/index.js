6a9ec0725012b9a2155ff29353ff2262a838020a
Aric Alves <aric.alves@yahoo.com>
2018-03-16 16:19:53 -0700

    (doc) Add docs for lib/data-sf

--- src/lib/data-sf/index.js
+++ src/lib/data-sf/index.js
@@ -1,15 +1,26 @@
 import axios from 'axios';
 import pick from 'lodash.pick';
 
+/* Simple GET request to data.sfgov */
 const fetchParklets = () => axios.get('https://data.sfgov.org/resource/6a7x-cttf.json');
 
+/**
+ * Returns a blob of parklet data.
+ * @returns {object[]} payload - A list of each parklet in SF.
+*/
 export async function getParklets() {
   const payload = await fetchParklets();
   return payload;
 }
 
+/**
+ * Prunes a parklet object and returns new object with only required keys.
+ * @param {object} parklet - Individual parklet object with many keys.
+ * @constant {array} keys - A list of keys on each blob that are required.
+ * @returns {object} - New object with only defined keys.
+ */
 export function parseParklet(parklet) {
-  const keys = [
+  const desiredKeys = [
     'applicant',
     'cross_street_1',
     'cross_street_2',
@@ -18,5 +29,5 @@ export function parseParklet(parklet) {
     'location',
     'street_name',
   ];
-  return pick(parklet, keys);
+  return pick(parklet, desiredKeys);
 }
