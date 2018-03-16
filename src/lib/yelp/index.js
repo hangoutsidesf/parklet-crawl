6f061677a501888ace36b934d95a26258e402add
Aric Alves <aric.alves@yahoo.com>
2018-03-16 12:58:48 -0700

    (refactor) Create Yelp class

--- src/lib/yelp/index.js
+++ src/lib/yelp/index.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.yelp.com/v3/businesses/';
+export default class Yelp {
+  constructor() {
+    this.BASE_URL = 'https://api.yelp.com/v3/businesses/';
+  }
 
-async function searchById(id) {
-  const data = await axios.get(BASE_URL + id, {
-    headers: {
-      Authorization: `Bearer ${process.env.YELP_API_KEY}`,
-    },
-  });
-  return data;
+  async searchById(id) {
+    const data = await axios.get(this.BASE_URL + id, {
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+      },
+    });
+    return data;
+  }
 }
-
-searchById('four-barrel-coffee-san-francisco')
-  .then(({ data }) => data)
-  .catch((err) => {
-    console.error(err);
-  });
