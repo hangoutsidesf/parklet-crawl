9259bedd9273c8fdcb8d1298207ac79eb067928d
Aric Alves <aric.alves@yahoo.com>
2018-03-16 15:13:47 -0700

    (doc) Add JSDoc style comments

--- src/lib/yelp/index.js
+++ src/lib/yelp/index.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 
+/* Yelp API module. */
 export default class Yelp {
   constructor() {
     this.BASE_URL = 'https://api.yelp.com/v3/businesses/';
   }
-
+  /**
+   * Returns more detailed information about a specific business.
+   * @param {string} id - The Yelp ID of business.
+   * @return {object} businessInformation - The author of the book.
+   */
   async searchById(id) {
-    const data = await axios.get(this.BASE_URL + id, {
+    const businessInformation = await axios.get(this.BASE_URL + id, {
       headers: {
         Authorization: `Bearer ${process.env.YELP_API_KEY}`,
       },
     });
-    return data;
+    return businessInformation;
   }
 }
