a972c3f266651750cad0153be7999a50177d689b
Aric Alves <aric.alves@yahoo.com>
2018-03-16 16:09:31 -0700

    (feat) Add function to parse parklet data blob

--- src/lib/data-sf/index.js
+++ src/lib/data-sf/index.js
@@ -1,8 +1,22 @@
 import axios from 'axios';
+import pick from 'lodash.pick';
 
 const fetchParklets = () => axios.get('https://data.sfgov.org/resource/6a7x-cttf.json');
 
-export default async function getParklets() {
+export async function getParklets() {
   const payload = await fetchParklets();
   return payload;
+}
+
+export function parseParklet(parklet) {
+  const keys = [
+    'applicant',
+    'cross_street_1',
+    'cross_street_2',
+    'envista_description',
+    'envista_intermediate_location_text',
+    'location',
+    'street_name',
+  ];
+  return pick(parklet, keys);
 }
