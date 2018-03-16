dad6b4289f728f5abced0479eecfd8fdb48f5ad9
Aric Alves <aric.alves@yahoo.com>
2018-03-16 16:10:00 -0700

    (test) add shallow test for #parseParklet()

--- spec/lib/data-sf/index.spec.js
+++ spec/lib/data-sf/index.spec.js
@@ -1,7 +1,13 @@
-import getParklets from '../../../src/lib/data-sf';
+import { getParklets, parseParklet } from '../../../src/lib/data-sf';
 
 describe('Data SF', () => {
   test('Should fetch blob of parklet data', () => (
     getParklets().then(({ data }) => expect(data.length).toBeGreaterThan(0))
+  ));
+  test('Should prune unecessary keys from parklet obj', () => (
+    getParklets()
+      .then(({ data }) => data[0])
+      .then(parklet => parseParklet(parklet))
+      .then(prunedParklet => expect(Object.keys(prunedParklet).length).toBe(7))
   ));
 });
