dd377b0407a11a48b2e09ba96e44f0db52d58ba6
Aric Alves <aric.alves@yahoo.com>
2018-03-16 17:23:46 -0700

    (feat) Add function to search for business id's

--- src/lib/yelp/index.js
+++ src/lib/yelp/index.js
@@ -4,13 +4,36 @@ const BASE_URL = 'https://api.yelp.com/v3/businesses/';
 
 /**
  * Returns more detailed information about a specific business.
+ * @async
  * @param {string} id - The Yelp ID of business.
- * @return {object} businessInformation - The author of the book.
+ * @constant {string} BASE_URL - The base url of the Yelp API.
+ * @return {Promise} resolve = businessInformation - Detailed business information.
  */
-export default async function searchById(id) {
+export async function searchById(id) {
   const businessInformation = await axios.get(BASE_URL + id, {
     headers: {
       Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+    },
+  });
+  return businessInformation;
+}
+
+/**
+ * Returns general information about a specific business.
+ * @async
+ * @param {string} name - The name of business.
+ * @return {Promise} resolve = businessInformation - General business information.
+ */
+export async function searchForBusiness(name) {
+  const businessInformation = await axios.get(`${BASE_URL}matches/best`, {
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+    },
+    params: {
+      name,
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'US',
     },
   });
   return businessInformation;
