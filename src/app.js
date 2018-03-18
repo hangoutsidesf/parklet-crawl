eb8ff841a2abedbb465b9af3190ad5a72a2f65ed
Aric Alves <aric.alves@yahoo.com>
2018-03-18 15:32:03 -0700

    (feat) Add cors module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { json as parseJSON } from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(parseJSON());
 
 app.all('*', (req, res) => res.sendStatus(200));
