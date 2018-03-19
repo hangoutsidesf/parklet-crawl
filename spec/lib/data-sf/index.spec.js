b5d62ad8acc17b6edacbddfd90384bb7401eaeff
andino <andy.andino93@gmail.com>
2018-03-19 14:08:18 -0700

    (tests) update tests to accept data without destructuring from #getParklets

--- spec/lib/data-sf/index.spec.js
+++ spec/lib/data-sf/index.spec.js
@@ -3,15 +3,15 @@ import { getParklets, parseParklet } from '../../../src/lib/data-sf';
 describe('Data SF', () => {
   describe('#getParklets()', () => {
     test('Should fetch blob of parklet data', () => (
-      getParklets().then(({ data }) => expect(data.length).toBeGreaterThan(0))
+      getParklets().then(data => expect(data.length).toBeGreaterThan(0))
     ));
   });
 
   describe('#parseParklet()', () => {
     test('Should prune unecessary keys from parklet obj', () => {
-      const index = Math.floor(Math.random() * 31); // Pick a random parklet from the list.
+      const index = Math.floor(Math.random() * 31 ); // Pick a random parklet from the list.
       return getParklets()
-        .then(({ data }) => data[index])
+        .then(data => data[index])
         .then(parklet => parseParklet(parklet))
         .then(prunedParklet => expect(Object.keys(prunedParklet).length).toBe(7));
     });
