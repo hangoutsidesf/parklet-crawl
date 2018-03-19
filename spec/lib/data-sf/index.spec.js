841ac609f80a1ecf9fdfa6e9741fefe85b9cbc25
Aric Alves <aric.alves@yahoo.com>
2018-03-18 19:03:01 -0700

    (refactor) Add 'describe' block for each method

--- spec/lib/data-sf/index.spec.js
+++ spec/lib/data-sf/index.spec.js
@@ -1,14 +1,19 @@
 import { getParklets, parseParklet } from '../../../src/lib/data-sf';
 
 describe('Data SF', () => {
-  test('Should fetch blob of parklet data', () => (
-    getParklets().then(({ data }) => expect(data.length).toBeGreaterThan(0))
-  ));
-  test('Should prune unecessary keys from parklet obj', () => {
-    const index = Math.floor(Math.random() * 31); // Pick a random parklet from the list.
-    return getParklets()
-      .then(({ data }) => data[index])
-      .then(parklet => parseParklet(parklet))
-      .then(prunedParklet => expect(Object.keys(prunedParklet).length).toBe(7));
+  describe('#getParklets()', () => {
+    test('Should fetch blob of parklet data', () => (
+      getParklets().then(({ data }) => expect(data.length).toBeGreaterThan(0))
+    ));
+  });
+
+  describe('#parseParklet()', () => {
+    test('Should prune unecessary keys from parklet obj', () => {
+      const index = Math.floor(Math.random() * 31); // Pick a random parklet from the list.
+      return getParklets()
+        .then(({ data }) => data[index])
+        .then(parklet => parseParklet(parklet))
+        .then(prunedParklet => expect(Object.keys(prunedParklet).length).toBe(7));
+    });
   });
 });
